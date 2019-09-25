74c5bfe1577eda6d795c16af3e4b0f6a172aa21c
VictorReyna <victor.reyna313@gmail.com>
2019-09-25 16:52:36 -0500

    Add files via upload

---
+++ 1_PthreadBarrier.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h> 
+#include <pthread.h>  
+////#include <chrono>
+
+int size;  
+int *data;  
+pthread_barrier_t our_barrier; 
+
+void *fun ( void *args )
+{
+   int *id = (int*) args;
+   int r = 1 + (rand() % 6);
+   int k;
+   char strd[size+1];
+
+   ///std::chrono::milliseconds timespan(r); // or whatever
+
+   ///std::this_thread::sleep_for(timespan);
+ ///cout<<"r: "<<r<<endl;
+ /// printf("el valor de r es:%d \n",r);
+  sleep(r);
+   
+  printf("Thread %d has slept %d seconds ...\n", *id, r);
+  data[*id] = r;
+
+   pthread_barrier_wait(&our_barrier);
+   
+   for(k=0; k<size; k++) strd[k] = '0' + ((char) data[k]);
+   strd[size] = '\0';
+
+   printf("Thread %d has data : %s\n", *id, strd);
+}
+
+int main ( int argc, char* argv[] )
+{
+   ///printf("Give the number of threads : "); 
+   ///scanf("%d", &size); 
+   ///printf(" Se leyo d: %d\n", size);
+    int size = strtol(argv[1], NULL, 10);
+   
+   data = (int*) calloc(size, sizeof(int));
+   {
+      pthread_t t[size];
+      pthread_attr_t a;
+      int id[size], i;
+
+      pthread_barrier_init(&our_barrier, NULL, size);
+
+      for(i=0; i<size; i++)
+      {
+         id[i] = i;
+         pthread_attr_init(&a);
+         if(pthread_create(&t[i], &a, fun, (void*)&id[i]) != 0)
+            printf("Unable to create thread %d!\n", i);
+      }
+      printf("Created %d threads ...\n", size);
+      for(i=0; i<size; i++) pthread_join(t[i], NULL);
+
+      pthread_barrier_destroy(&our_barrier);
+   }
+   return 0;
+}
